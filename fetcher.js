1cb84abc49c561b79c9356a03e8ffc13ab890f54
Umar Khan <umarkhan167@outlook.com>
2021-11-19 16:10:02 -0500

    fetcher file successfully operating

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const fs = require('fs')
+const fs = require('fs');
 
 let myArgs = process.argv.slice(2);
 
@@ -13,7 +13,7 @@ const fetchFile = function(url, localPath) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
-    console.log(response.statusCode)
+    console.log(response.statusCode);
     if (error) {
       return `Failed to get the resource from file: ${error}`;
     }
@@ -23,12 +23,12 @@ const fetchFile = function(url, localPath) {
       } else {
         console.log(`failed to write to ${localPath}`);
       }
-    })
+    });
   });
-}
+};
 if (!url || !localPath) {
-  console.log("Please pass in two arguments.")
-  console.log("node fetcher.js <url> <localPath>")
+  console.log("Please pass in two arguments.");
+  console.log("node fetcher.js <url> <localPath>");
 } else {
   fetchFile(url, localPath);
 }
